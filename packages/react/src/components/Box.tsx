beba0d4b02860ef4cc32e33b30f00642b363d548
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2022-11-02 10:53:56 -0300

    fix: box prop

--- packages/react/src/components/Box.tsx
+++ packages/react/src/components/Box.tsx
@@ -5,8 +5,6 @@ export const Box = styled('div', {
   background: '$white',
   outline: '1px solid',
   outlineColor: '$gray200',
-  width: '$16',
-  height: '$16',
   padding: '$2 $4',
   borderRadius: '$md',
 })
